d3645d108f1dc2c9f4166fdbfceb9f5db791c4e6
James Flanagan <james.ilkley@gmail.com>
2014-12-12 19:41:13 +0000

    Reorganised code

--- bitshower.py
+++ bitshower.py
@@ -3,123 +3,120 @@
 
 # TODO: add some more comments!
 
-#import audio module, 'wave' sound file module, and multiple thread module
-import pyaudio, wave, threading
-#import some stuff from the random module
-from random import randint, randrange
-#import module to find console size
-import console_size
-#the consol_size and pyaudio modules can be accessed from the files above.
 
-#globalise constants
-#used for matrix generator (main)
-global CONSOLE_WIDTH
-global CONSOLE_HEIGHT
+# * pyaudio is used for playing the background music.
+# * wave is used for reading the wav file that contains the background music.
+# * threading is used to run the program on multiple threads at once, so the audio can be played while the animation is displayed.
+import pyaudio, wave, threading
+
+from random import randint, randrange
+
+
+# console_size is used to find out the size of the window the program is running in.
+import console_size
+
+# Get the size of the console.
 (CONSOLE_WIDTH, CONSOLE_HEIGHT) = console_size.getTerminalSize()
 
-global MAX_LENGTH
+
+# CONFIGURATION FOR ANIMATION #
+# =========================== #
+
 MAX_LENGTH = 10
 
-global MAX_CHANGE_EACH_TIME
 MAX_CHANGE_EACH_TIME = int(CONSOLE_WIDTH / 5)
 
-#used for sound
-global CHUNK
+#    END OF CONFIGURATION     #
+#    --------------------     #
+
+
+# Used for sound generation.
 CHUNK = 1024
 
-#random bit lambda function
+# Used to generate a random bit to display on the screen in the animation.
 randbit = lambda: randint(0, 1)
 
-
 def generateLines():
-    """generates something that's used in procedure main
-    jfla can explain it"""
+    """Generate a random set of column numbers of random length, each of which respresents a line going down the screen. This can be used to create a list of lines to remove from, or add to the screen."""
     lines = set()
 
+    # Generate the number of lines to be chosen, which must be between 1 and MAX_CHANGE_EACH_TIME.
     numToChange = randint(1, MAX_CHANGE_EACH_TIME)
 
+    # Generate the column numbers for the lines.
     for i in range(numToChange):
         lines.add(randrange(CONSOLE_WIDTH))
-    
+
     return lines
-    
-    
-class Application():
-    """a class with most of the stuff for running the program in it"""
-    
-    def __init__(self):
-        """runs when an application object is created"""
-        # instantiate PyAudio
-        self.p = pyaudio.PyAudio()
-        """get sound from file and
-        create a stream to stream it on"""
-        self.sound()
-        #something to do with matrix generator
-        self.lines = set()
-        #begin program
-        self.begin()
 
-    def sound(self):
-        """gets sound from a file"""
-        #use wave module to open a wav file
-        self.sounds = wave.open('/Users/glenmcconkey/Documents/Almanzos_folder/Python_music/matrix.wav', 'rb')
-        # open stream
-        self.stream = self.p.open(format=self.p.get_format_from_width(self.sounds.getsampwidth()),
-                channels=self.sounds.getnchannels(),
-                rate=self.sounds.getframerate(),
-                output=True)
+def displayAnimation():
+    """Displays the actual animation."""
+
+    lines = set()
+
+    # Loop forever.
+    while True:
+        if len(lines) > 0 + MAX_CHANGE_EACH_TIME:
+            lines -= generateLines()
+
+        if len(lines) < CONSOLE_WIDTH - MAX_CHANGE_EACH_TIME:
+            lines |= generateLines()
+
+        linesList = list(lines)
+
+        linesList.sort()
+
+        for i, l in enumerate(linesList):
+            if i == 0:
+                numSpacesToAdd = l
+            else:
+                # Have to -1 since one of the columns will have already been used up
+                # by the previous line.
+                numSpacesToAdd = l - linesList[i-1] - 1
+
+            print(" " * numSpacesToAdd + str(randbit()), end='')
+
+        print()
+
+def playMusic():
+    """Loops a sound widget"""
+
+    def readSoundFile():
+        """Gets sound from the file."""
+
+        # Use wave module to open the wav file.
+        return wave.open('sound.wav', 'rb')
+
+    sounds = readSoundFile()
+
+    # Initialise pyaudio instance.
+    pa = pyaudio.PyAudio()
+
+    # Open output stream.
+    stream = pa.open(format=pa.get_format_from_width(sounds.getsampwidth()),
+                   channels=sounds.getnchannels(),
+                       rate=sounds.getframerate(),
+                     output=True)
+
+    # Play the sound effect forever.
+    while True:
+        # Read the next chunk of data from the wav file.
+        data = sounds.readframes(CHUNK)
+
+        # While data is being read.
+        while data != '':
+            # Write data onto the stream to play it.
+            stream.write(data)
+
+            # Read the next chunk of data from the wav file.
+            data = sounds.readframes(CHUNK)
+
+        # Rewind the track to the beginning.
+        sounds.rewind()
 
 
-    def playMusic(self): 
-        """loops a sound widget"""
-        #loop forever
-        while True:
-            # read the next chunk of data from the wav file
-            self.data = self.sounds.readframes(CHUNK)
-            #while data is being read
-            while self.data != '':
-                #write data onto the stream to play it
-                self.stream.write(self.data)
-                #read the next chunk of data from the wav file
-                self.data = self.sounds.readframes(CHUNK)
-            #rewind the track to the beginning
-            self.sounds.rewind()
-        
-        
-    def main(self):
-        """runs the matrix generator part of the program"""
-        #loops forever
-        while True:
-            #ask jfla how this bit works
-            if len(self.lines) > 0 + MAX_CHANGE_EACH_TIME:
-                self.lines -= generateLines()
-        
-            if len(self.lines) < CONSOLE_WIDTH - MAX_CHANGE_EACH_TIME:
-                self.lines |= generateLines()
+# Create and run a second thread which plays the sound.
+threading.Thread(target = playMusic).start()
 
-            linesList = list(self.lines)
-
-            linesList.sort()
-
-            for i, l in enumerate(linesList):
-                if i == 0:
-                    numSpacesToAdd = l
-                else:
-                    # Have to -1 since one of the columns will have already been used up
-                    # by the previous line.
-                    numSpacesToAdd = l - linesList[i-1] - 1
-
-                print(" " * numSpacesToAdd + str(randbit()), end='')
-
-            print()
-    
-    def begin(self):
-        """runs the whole program"""
-        #create and run a second thread which plays the sound
-        threading.Thread(target = self.playMusic).start()
-        #run the matrix generator part of the program
-        self.main()
-
-#create an object to start the program
-Application()
-
+# Begin your hacking adventure!
+displayAnimation()
